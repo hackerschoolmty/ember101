8866f8646f9cb7164b66d2f06731c05168c7d432
Amed Rodriguez <amed.rdz@gmail.com>
2015-07-04 08:45:37 -0500

    Implement autosave on articles list

--- 05/practice-final/app/components/articles/article-row.js
+++ 05/practice-final/app/components/articles/article-row.js
@@ -3,9 +3,21 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'tr',
   article: null,
-  actions: {
-    saveArticle: function(article) {
+  articleStates: null,
+
+  autoSave: function() {
+    var article = this.get('article');
+
+    if (!article.get('isNew')) {
       this.sendAction('save', article);
     }
-  }
+  },
+
+  stateChanged: Ember.on('init', Ember.observer('article.state', function() {
+    var article = this.get('article');
+
+    if (article.get('isDirty') && !article.get('isSaving')) {
+      Ember.run.once(this, this.autoSave);
+    }
+  }))
 });
