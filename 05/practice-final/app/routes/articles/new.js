1e858211c4423856838aa519738743a5f47f47e5
bhserna <bhserna@gmail.com>
2015-07-04 01:19:28 -0500

    wip: lend articles with default attribute in route

---
+++ 05/practice-final/app/routes/articles/new.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function() {
+    return this.store.createRecord("article", {
+      state: "borrowed",
+      friend: this.modelFor("friends.show")
+    });
+  },
+
+  actions: {
+    save: function() {
+      var _this = this;
+      var model = this.modelFor("articles.new");
+
+      model.save().then(function() {
+        this.transitionTo("articles");
+      });
+    },
+
+    cancel: function() {
+      this.transitionTo("articles");
+    }
+  }
+});
